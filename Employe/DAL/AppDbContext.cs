7eb9f75a8e4b747a68a2fe6d8283028efef86a18
huseynhesenov1 <huseynzh-ab205code.edu.az>
2024-11-29 17:19:43 +0400

    first commit

---
+++ Employe/DAL/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Employe.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employe.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Master> Masters { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Order>()
+               .HasOne(e => e.Master)
+               .WithMany(e => e.Orders)
+               .HasForeignKey(e => e.MasterId)
+               .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Master>()
+              .HasOne(e => e.Order)
+              .WithMany(e => e.Masters)
+              .HasForeignKey(e => e.MasterId)
+              .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ Employe/Program.cs
@@ -0,0 +1,27 @@
+using Employe.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employe
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+
+            builder.Services.AddDbContext<AppDbContext>(
+               options =>
+               options.UseSqlServer(builder.Configuration.GetConnectionString("MsSql")));
+            var app = builder.Build();
+            app.UseStaticFiles();
+
+
+            app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.Run();
+        }
+    }
+}
