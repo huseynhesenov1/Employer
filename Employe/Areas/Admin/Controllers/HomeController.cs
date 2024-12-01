5b793c26b6e4a7a83d4fc24334c01393a572839c
huseyn-dev <huseyn.developer.15@gmail.com>
2024-12-01 19:23:36 +0400

    added  orders table

--- Employe/Areas/Admin/Controllers/HomeController.cs
+++ Employe/Areas/Admin/Controllers/HomeController.cs
@@ -1,12 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Employe.DAL;
+using Employe.Models;
+using Employe.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Employe.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class HomeController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Order> orders =  _context.Orders.ToList();
+            return View(orders);
         }
+        [HttpGet]
+        public IActionResult Create()
+        {
+  
+            ViewBag.Services = new SelectList(_context.Services, "Id", "Title");
+            ViewBag.Masters = new SelectList(_context.Masters.Where(m => m.IsActive == true), "Id", "Name");
+
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(OrderVM ordervm)
+        {
+            if (ModelState.IsValid)
+            {
+                Order order = new Order()
+                {
+                    ClientName = ordervm.ClientName,
+                    ClientSurname = ordervm.ClientSurname,
+                    ClientPhoneNumber = ordervm.ClientPhoneNumber,
+                    ClientEmail = ordervm.ClientEmail,
+                    ServiceId = ordervm.ServiceId,
+                    MasterId = ordervm.MasterId,
+                    Problem = ordervm.Problem,
+                    IsActive = ordervm.IsActive,
+                    CreatedAt = DateTime.Now,
+
+                };
+
+                _context.Orders.Add(order);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Services = new SelectList(_context.Services, "Id", "Title");
+            ViewBag.Masters = new SelectList(_context.Masters.Where(m => m.IsActive == true), "Id", "Name");
+
+            return View(ordervm);
+        }
+
     }
 }
---
+++ Employe/ViewModels/OrderVM.cs
@@ -0,0 +1,24 @@
+﻿using Employe.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Employe.ViewModels
+{
+    public class OrderVM
+    {
+       
+        public string ClientName { get; set; }
+        public string ClientSurname { get; set; }
+        public string ClientPhoneNumber { get; set; }
+        public string ClientEmail { get; set; }
+        public int ServiceId { get; set; }
+       
+        public int MasterId { get; set; }
+        
+
+        public string Problem { get; set; }
+
+        public bool IsActive { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
